566bafa51d775610e4e421ab983bb07b7532aa06
Hasan alkholy <hassan.m.h.m.a123@gmail.com.com>
2021-03-23 14:49:17 +0200

    finished

---
+++ js/main.js
@@ -0,0 +1,172 @@
+$(window).on("load", function () {
+  //preloader
+  $(".pre-loader").fadeOut("500", function () {
+    $("body").removeClass("overflow");
+    $(this).remove();
+  });
+
+  if ($(".pre-loader").length == 0) {
+    $("body").removeClass("overflow");
+  }
+
+  // animate on scroll
+
+  AOS.init({
+    duration: 700,
+    mirror: true,
+    once: true,
+    disable: function () {
+      var maxWidth = 992;
+      return window.innerWidth < maxWidth;
+    },
+    easing: "ease-in-out",
+  });
+
+  //nava toggle
+  $("#nava-icon").click(function (e) {
+    $("#nava").toggleClass("nava-active");
+    $("html").toggleClass("overflow");
+  });
+
+  $("#nava").click(function (e) {
+    if (
+      e.target.id == "nava" ||
+      e.target.id == "close-nava" ||
+      e.target.parentNode.id == "close-nava"
+    ) {
+      $(this).removeClass("nava-active");
+      $("html").removeClass("overflow");
+    }
+  });
+
+  //search toggle
+  $("#search-btn").click(function (e) {
+    $("#search-form").toggleClass("search-form-active");
+    $("html").toggleClass("overflow");
+  });
+
+  $("#search-form").click(function (e) {
+    if (
+      e.target.id == "search-form" ||
+      e.target.id == "close-search" ||
+      e.target.parentNode.id == "close-search"
+    ) {
+      $(this).removeClass("search-form-active");
+      $("html").removeClass("overflow");
+    }
+  });
+
+  $(".slide").on("click", function (e) {
+    if (
+      (e.target.classList.contains("drop") &&
+        e.target.parentNode.classList.contains("slide")) ||
+      e.target.classList.contains("slide")
+    ) {
+      e.stopPropagation();
+      $(this).toggleClass("slide-active");
+      $(this).children(".top-dropDown").slideToggle();
+    }
+  });
+
+  function changeSlide() {
+    if (window.innerWidth <= 1200) {
+      $(".sm-slide").removeClass("top-setting");
+      $(".sm-slide").addClass("slide");
+      $(".sm-slide .top-dropDown").css("display", "none");
+    } else {
+      $(".sm-slide").addClass("top-setting");
+      $(".sm-slide").removeClass("slide");
+      $(".sm-slide .top-dropDown").css("display", "flex");
+    }
+  }
+
+  changeSlide();
+
+  window.addEventListener("resize", changeSlide);
+
+  // add footer collabse
+  function addCollapse() {
+    if (window.innerWidth <= 992) {
+      $(".footer-list").attr("data-toggle", "collapse");
+      $(".footer-list ul").addClass("collapse");
+    } else {
+      $(".footer-list").attr("data-toggle", "none");
+      $(".footer-list ul").removeClass("collapse");
+      $(".footer-list ul").css("height", "auto");
+    }
+  }
+
+  addCollapse();
+  window.addEventListener("resize", addCollapse);
+
+  $(".footer-list").on("click", function (e) {
+    if (window.innerWidth <= 992) {
+      e.preventDefault();
+      $(this).toggleClass("footer-list-active");
+    }
+  });
+
+  var mySwiper = new Swiper("header .swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    effect: "fade",
+    loop: true,
+    updateOnWindowResize: true,
+    autoplay: {
+      delay: 5000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  window.addEventListener("scroll", scrolled);
+
+  function scrolled() {
+    let up = document.getElementById("up");
+    if (this.scrollY > 200) {
+      up.classList.add("active-up");
+    } else {
+      up.classList.remove("active-up");
+    }
+  }
+
+  //counter
+  var a = 0;
+  $(window).scroll(function () {
+    var oTop = $("#counter").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $(".counter-value").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 2000,
+            easing: "linear",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+      a = 1;
+    }
+  });
+});
